67683f3241543f22163e9e8b0db30bee7588caae
Denz-afk <78831788+Denz-afk@users.noreply.github.com>
2021-02-27 20:38:42 -0400

    Add files via upload

---
+++ Program1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] debm = new int[4];
+
+            for(int i=0; i<debm.Length; i++)
+            {
+                Console.WriteLine("Escribe un número");
+                int num = Convert.ToInt32(Console.ReadLine());
+                debm[i] = num;
+            }
+
+            int debm1 = 0;
+            for (int i = 0; i<debm.Length; i++)
+            {
+                debm1 += debm[i];
+            }
+
+            Double debm2 = (Double) debm1 / debm.Length;
+
+            Console.WriteLine("El promedio es: "+debm2);
+            Console.WriteLine("Los datos ingresados fueron: ");
+            for(int i=0; i<debm.Length; i++)
+            {
+                Console.WriteLine(debm[i]);
+            }
+        }
+    }
+}
