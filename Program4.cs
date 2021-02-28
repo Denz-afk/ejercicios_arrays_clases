715b8647b955bd38586daaf97c9c931bfde72c15
Denz-afk <78831788+Denz-afk@users.noreply.github.com>
2021-02-27 20:50:40 -0400

    Add files via upload

---
+++ Program4.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] debm = new int[10];
+            
+            for(int i=0; i<debm.Length; i++)
+            {
+                Console.WriteLine("Escribe un número");
+                int num = Convert.ToInt32(Console.ReadLine());
+                debm[i] = num;
+            }
+
+            Array.Sort(debm);
+
+            int debm1 = debm[debm.Length - 1];
+
+            Console.WriteLine("El número mayor es: " + debm1);
+        }
+    }
+}
