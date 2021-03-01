de88ffa12aaa7e9f102de1016e9c2409078a235c
Denz-afk <78831788+Denz-afk@users.noreply.github.com>
2021-03-01 01:58:05 -0400

    Add files via upload

---
+++ Program2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int debm1 = 0;
+            float[] numeros = new float[5];
+            do
+            {
+                Console.WriteLine("Escribe un número: ");
+                numeros[debm1] = float.Parse(Console.ReadLine());
+                debm1++;
+            }
+            while (debm1 < 5);
+            if (debm1 >= 5)
+            {
+                Array.Reverse(numeros);
+                Console.WriteLine("El orden contrario es: ");
+                for (int i = 0; i < numeros.Length; i++)
+                {
+                    Console.WriteLine(numeros[i]);
+                }
+            }
+        }
+    }
+}
---
+++ Program3.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] debm = new int[13];
+            
+            debm[1] = 31;
+            debm[2] = 28;
+            debm[3] = 31;
+            debm[4] = 30;
+            debm[5] = 31;
+            debm[6] = 30;
+            debm[7] = 31;
+            debm[8] = 31;
+            debm[9] = 30;
+            debm[10] = 31;
+            debm[11] = 30;
+            debm[12] = 31;
+
+            Console.WriteLine("ingrese el numero del mes: ");
+            int debm1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Tiene " + debm[debm1] + " días ");
+
+            Console.ReadKey();
+        }
+    }
+}
